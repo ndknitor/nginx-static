fb9295260a9a468ce5c8b27f29e82f4893b4a428
ndknitor <nguyentuanpkn@gmail.com>
2024-05-16 11:03:23 +0700

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,9 +13,63 @@ if [ ! -e "$ssl_folder/$crt_file" ] || [ ! -e "$ssl_folder/$key_file" ]; then
 fi
 
 if [ "$INDEX_WILDCARD_FORWARDING" = "1" ]; then
-    sed -i '/location \/ {/a \
-        try_files $uri $uri\/ \/index.html;
-    ' "$nginx_conf"
-fi
+        echo "
+user  nginx;
+worker_processes  auto;
 
+error_log  /var/log/nginx/error.log notice;
+pid        /var/run/nginx.pid;
+
+events {
+    worker_connections  $worker_connections;
+}
+http {
+    include       /etc/nginx/mime.types;
+    default_type  application/octet-stream;
+    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+                      '\$status \$body_bytes_sent "\$http_referer" '
+                      '"\$http_user_agent" "\$http_x_forwarded_for"';
+    access_log  /var/log/nginx/access.log  main;
+    sendfile        on;
+    keepalive_timeout  65;
+    server {
+        listen       80;
+        listen  [::]:80;
+        server_name  localhost;
+        #access_log  /var/log/nginx/host.access.log  main;
+        location / {
+            root   /app;
+            try_files \$uri \$uri/ /index.html;
+            index  index.html index.htm;
+        }
+        error_page   500 502 503 504  /50x.html;
+        location = /50x.html {
+            root   /app;
+        }
+    }
+
+
+    server {
+        listen       443 ssl;
+        listen  [::]:443 ssl;
+        server_name  localhost;
+        ssl_certificate /ssl/app.crt;
+        ssl_certificate_key /ssl/app.key;
+        #access_log  /var/log/nginx/host.access.log  main;
+        location / {
+            root   /app;
+            try_files \$uri \$uri/ /index.html;
+            index  index.html index.htm;
+        }
+        #error_page  404              /404.html;
+        # redirect server error pages to the static page /50x.html
+        #
+        error_page   500 502 503 504  /50x.html;
+        location = /50x.html {
+            root   /app;
+        }
+    }
+
+}" > "$nginx_conf"
+fi
 nginx -g 'daemon off;'
